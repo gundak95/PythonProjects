d43489deb4ebe902adfdacde729bf0523c999474
gundak95 <gunda.kinzlgk@gmail.com>
2022-07-29 21:30:04 +0200

    Fixed it

--- numbers.py
+++ numbers.py
@@ -1,5 +1,8 @@
-answer=input("\nWhich number do you like?") 
-if answer>2
-    print(f"{answer} is greater than two!")
+from tkinter import X
+
+
+x=input("\nWhich number do you like?") 
+if int(x) > 2:
+    print(f"{x} is greater than two!")
 else:
-    print(f"{answer} isn't greater than two.")
+    print(f"{x} isn't greater than two.")
