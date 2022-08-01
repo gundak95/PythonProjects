f2ecc1be35b0764a6be338872e1a969e8ab7dd3c
gundak95 <gunda.kinzlgk@gmail.com>
2022-08-01 16:38:49 +0200

    created document to try parsing xml with python

---
+++ parsing_xml.py
@@ -0,0 +1,8 @@
+#This is my first actually really useful Python file for changing stuff in xml with Regex.
+
+from xml.dom.minidom import parse, parseString
+document=parse("hello_image.svg")
+
+
+
+
