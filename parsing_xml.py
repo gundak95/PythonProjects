a1c06566f51fb8be647e547b041293a0824cf079
gundak95 <gunda.kinzlgk@gmail.com>
2022-08-01 17:05:51 +0200

    parsing method#1

--- parsing_xml.py
+++ parsing_xml.py
@@ -1,8 +1,6 @@
 #This is my first actually really useful Python file for changing stuff in xml with Regex.
 
-from xml.dom.minidom import parse, parseString
-document=parse("hello_image.svg")
-
-
-
-
+import xml.etree.ElementTree as ET
+mytree=ET.parse("sample.xml")
+myroot=mytree.getroot()
+print(myroot)
