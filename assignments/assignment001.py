3a1f0c2f3bfd0a1fe0f27d2bd19fa2e40cac4a9f
gundak95 <gunda.kinzlgk@gmail.com>
2022-08-27 12:09:21 +0200

    chore:Find out about shallow copy and apply
    Import copy module, make a copy of list and modify lists independently.

--- assignments/assignment001.py
+++ assignments/assignment001.py
@@ -1,3 +1,6 @@
+from copy import copy
+
+
 mylist = [1, 2, 3]
 print(mylist)
 myotherlist = mylist
@@ -10,7 +13,7 @@ print(myotherlist)
 #Then, however, I tell it that for this second list, the first number is 5.
 #What I get when I print both lists is two times the same: 5, 2, and 3.
 #The reason for this must be that Python thinks when I say myotherlist is mylist,
-#then I mean that also mylist is myotherlist. This is a shallow copy.
+#then I mean that also mylist is myotherlist.
 #Changes to any one of them will cause changes in both.
 #In this simple example, to change this behaviour, I can tell it to print mylist before I say that myotherlist is essentially the same, see above.
 
@@ -23,4 +26,17 @@ print("List for store B", myotherlist)
 #Say, I have a list of fruits that is the same in stores, but in one of them,
 #the customers love peaches. I will then always get peaches for them and replace the
 #first other fruit on the list.
+
+import copy
+mylist = ["apples", "pears", "grapes"]
+myotherlist = copy.copy(mylist)
+myotherlist.append(["peaches", "plums", "mangoes"])
+print("My List", mylist)
+print("My Other List", myotherlist)
+
+#I found a nice tutorial for copies in python, so from here on,
+#this is not something I knew before or came up with off the top of my head.
+#Making a copy of the list instead of telling python that the lists are identical
+#allows me to alter one of the lists while leaving the other intact.
+#I added some items to the new list, but not the old one.
 
