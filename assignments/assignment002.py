a21823681575c032e88de3dc443b8a1041d98666
gundak95 <gunda.kinzlgk@gmail.com>
2022-08-28 17:47:37 +0200

    add: An empty file where 2nd assignment will be

--- assignments/assignment002.py
+++ assignments/assignment002.py
@@ -1,12 +1,1 @@
-import copy
-mylist = [["apples", "pears", "grapes"],["flour", "noodles", "rice"]]
-myotherlist = copy.copy(mylist)
-mylist[0][0] = 'bananas'
-myotherlist.append(["courgettes", "celery", "potatoes"])
-
-print("My List", mylist)
-print("My Other List", myotherlist)
-
-#This took me forever, because I could not figure out the missing comma between
-#"grapes"] and ["flour", but it works now. I replaced apples with bananas in both lists and added
-#vegetables only to the second.
+print("This is empty as of now.")
