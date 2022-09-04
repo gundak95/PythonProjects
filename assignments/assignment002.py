a0ff81bffed0d1d9b343472e45a49645b6ed02c3
gundak95 <gunda.kinzlgk@gmail.com>
2022-09-04 19:16:45 +0200

    chore: A weird intermediate stage of assignment002
    Used to have a while-loop that completely killed the program, now has a for-loop, let's see.

--- assignments/assignment002.py
+++ assignments/assignment002.py
@@ -1,9 +1,22 @@
+alternative_names = {
+    "Fabian","Fabi","Fabian Krautgasser","Krauthex","Krautgasser",
+}
+
 name=input("What is your name? ")
 
 if name == "Fabs":
     print(f"{name} is absolutely the name by which I know you.")
+
+for name in alternative_names:
+    print(f"Hm, I remember you being called something else than {name}.")
+
+
 else:
     print(f"Are you sure that {name} is your name?")
+
+    
+#if name == "Gunda"
+    #break
 
 age=input("How old are you? ")
 age=int(age)
