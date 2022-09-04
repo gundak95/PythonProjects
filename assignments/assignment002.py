bb7cbf164fc59378763ba91ea2887500b2914f31
gundak95 <gunda.kinzlgk@gmail.com>
2022-09-04 17:22:42 +0200

    add: A first simple try for assignment002
    Add a very simple first solution for the second assignment

--- assignments/assignment002.py
+++ assignments/assignment002.py
@@ -1,1 +1,8 @@
-print("This is empty as of now.")
+from tkinter.font import names
+
+
+name=input("Enter your name here: ")
+if name == "Fabs":
+ print(f"{name} is absolutely the name by which I know you.")
+else:
+    print("Are you sure that " f"{name} is your name?")
