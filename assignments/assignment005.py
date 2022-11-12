08f9be274642c2dba3d7afc93469eda6a9bc3623
gundak95 <gunda.kinzlgk@gmail.com>
2022-11-12 18:46:47 +0100

    delete: deleted newfile through script

--- assignments/assignment005.py
+++ assignments/assignment005.py
@@ -1,11 +1,14 @@
 f = open("newfile.txt", "w")
 f.write("This is new text in a brandnew file")
-f.close
+f.close()
 
 f = open("newfile.txt")
 print(f.readline())
+f.close()
 
 #This (almost) worked at first try.
 
 import os
 os.remove("newfile.txt")
+#This only works when you close the file first.
+#Closing only works when you put the two brackets after "close" (duh).
