fcaa58453d8414fd8b6fb02921f82e98efca8ca3
gundak95 <gunda.kinzlgk@gmail.com>
2022-11-13 19:50:40 +0100

    chore: play around, test opening w "with", "for".

--- assignments/assignment006.py
+++ assignments/assignment006.py
@@ -1,3 +1,28 @@
-f = open ("sample.txt")
-text = f.write("This is completely new text for the file.\n Here is also a linebreak in this text.")
-print(text)
+#f = open ("newsample.txt","w")
+#text = f.write("Why the heck is there no text in this file \n cause there sure should be.")
+#f.close()
+
+#f = open("newsample.txt")
+#text = f.readlines()
+#print(text)
+#f.close()
+
+#So far, so good. This is just a revision.
+
+f = open("newsample.txt")
+lines = f.readlines()
+
+#for line in lines:
+    #line = line.replace("\n","")
+    #print(line)
+#This I think does not do as it does here: https://pythonspot.com/read-file/
+#I don't know why.
+import os
+if not os.path.isfile("loremipsum.txt"):
+    print("The file does not exist.")
+else:
+    with open("loremipsum.txt") as text:
+        content = text.read().splitlines()
+    for line in content:
+        print(line),
+#Tested with typo in the filename, works like a charm.
