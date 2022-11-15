875000bd4781169485ff2c85ee8586d2b356816f
gundak95 <gunda.kinzlgk@gmail.com>
2022-11-15 18:43:01 +0100

    chore: make work, does work almost perfectly now

--- assignments/assignment007.py
+++ assignments/assignment007.py
@@ -1,10 +1,8 @@
-#word = input("What are you looking for? ")
+word = input("What are you looking for? ")
+
 with open('loremipsum.txt', 'r') as text:
-   # for line in text:
-    #    line = line.lower()
-    if word("ipsum") in text:
+    if word in text.readline().lower():
         print("Found it!")
-        break
     else:
         print("This isn't here.")
 #This works now. The search is case sensitive.
@@ -12,3 +10,4 @@ with open('loremipsum.txt', 'r') as text:
 #Managed to capitalize input, but then it capitalises every input and does not find lower-case words.
 #I can't seem to manage. This really starts to bother me.
 #It worked and I ruined it again.
+#Got it to work again. It also looks for parts of the word. I would like to be able to fix this.
