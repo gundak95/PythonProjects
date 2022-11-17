6f88e425df6ae5b9657747009d794fde9d4089fa
gundak95 <gunda.kinzlgk@gmail.com>
2022-11-17 16:52:03 +0100

    add: file for loops

--- automate_the_boring_stuff/chapter2.py
+++ automate_the_boring_stuff/chapter2.py
@@ -1,12 +1,38 @@
-print("Enter Name: ")
+#First program
+#print("Enter Name: ")
+#name=input()
+#if name == "Mary":
+    #print("Hello, Mary. We have been waiting for you.")
+    #print("Enter your password, Mary. ")
+    #password=input()
+    #if password == "Starfish":
+        #print("You may enter.")
+    #else:
+        #print("You are not Mary.")
+#else:
+    #print(f"Hello, {name}. You cannot enter here.")
+
+#Second program
+#pretty_names=["Alice","Mary","Diddeldum"]
+#name=input()
+#if name in pretty_names:
+    #print(f"Hi, {name}!")
+#else:
+    #print("What a name!")
+
+#Third program
+print("Hello. What's your name?")
 name=input()
-if name == "Mary":
-    print("Hello, Mary. We have been waiting for you.")
-    print("Enter your password, Mary. ")
-    password=input()
-    if password == "Starfish":
-        print("You may enter.")
-    else:
-        print("You are not Mary.")
+print(f"Hello, {name}. How old are you?")
+age=input()
+age=int(age)
+if name == "Alice" and age == 27:
+    print("Oh, hi Alice!")
+elif age < 27:
+    print("You are not Alice, kiddo.")
+elif age >= 100 and age <= 1000:
+    print("You are not Alice, granny.")
+elif age > 1000:
+    print("Impossible! Vampires do not exist!")
 else:
-    print(f"Hello, {name}. You cannot enter here.")
+    print("Okay, but this program is only for Alice.")
