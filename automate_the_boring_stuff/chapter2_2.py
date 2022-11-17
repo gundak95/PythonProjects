3caec1dbea0d50b3a2656bd4b862d3c6c28ed0e5
gundak95 <gunda.kinzlgk@gmail.com>
2022-11-17 17:01:38 +0100

    chore: write little program with loops

---
+++ automate_the_boring_stuff/chapter2_2.py
@@ -0,0 +1,11 @@
+while True:
+    print("Enter your name, gnome!")
+    name = input()
+    if name != "Gundibor":
+        print("Psh, you are the wrong gnome and just broke your computer.")
+        continue
+    if name == "Gundibor":
+        break
+print("Hello, Gundibor. Please enter your password.")
+password = input()
+#Bit of redundancy still left, will fix next time.
