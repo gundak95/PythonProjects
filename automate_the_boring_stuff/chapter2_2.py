c82301bf6ae9451a17da640bb596841aee2bb01b
gundak95 <gunda.kinzlgk@gmail.com>
2022-11-18 10:25:19 +0100

    chore: explore Truthy and Falsey

--- automate_the_boring_stuff/chapter2_2.py
+++ automate_the_boring_stuff/chapter2_2.py
@@ -1,26 +1,26 @@
-while True:
-    print("Enter your name, gnome!")
-    name = input()
-    if name == "Gundibor":
-        break
-    print("Psh, you are the wrong gnome and just broke your computer.")
+#while True:
+#    print("Enter your name, gnome!")
+#    name = input()
+#    if name == "Gundibor":
+#        break
+#    print("Psh, you are the wrong gnome and just broke your computer.")
 
 
-print('Hello, Gundibor.')
-password=''
-while password != '1234':
-    print("Enter your password.")
-    password = input()
-    print('Wrong! Go back to the cave from which you crawled, impostor.')
+#print('Hello, Gundibor.')
+#password=''
+#while password != '1234':
+#    print("Enter your password.")
+#    password = input()
+#    print('Wrong! Go back to the cave from which you crawled, impostor.')
 
-print('You are the real Gundibor and may enter.')
+#print('You are the real Gundibor and may enter.')
 
-while True:
-    print('Now that I know who you are, tell me your secret.')
-    secret=input()
-    if secret != 'I am only a half-gnome.':
-        continue
-    print('I can tell.')
+#while True:
+#    print('Now that I know who you are, tell me your secret.')
+#    secret=input()
+#    if secret != 'I am only a half-gnome.':
+#        continue
+#    print('I can tell.')
 
 #Bit of redundancy still left, will fix next time.
 #This is basically two ways of doing the same thing. Either I break the loop when
@@ -36,4 +36,17 @@ while True:
 #Order is important. (Didn't get it right at first try.)
 #First use of an empty string! Didn't get how they worked before, very useful!
 
+#For Truthy and Falsey values:
+
+name = ''
+while not name:
+    print('Please enter your name: ')
+    name=input()
+
+print('How many guests do you expect?')
+number_of_guests=int(input())
+if number_of_guests:
+    print ('Shalala.')
+else:
+    print ('How sad.')
 
