8bd3df31195530ef82c748305a115b554a31b365
gundak95 <gunda.kinzlgk@gmail.com>
2022-12-05 17:36:11 +0100

    chore: chapter 3 tryouts

--- automate_the_boring_stuff/chapter3_1.py
+++ automate_the_boring_stuff/chapter3_1.py
@@ -4,7 +4,7 @@ def travelDestination(answerNumber):
     if answerNumber == 1:
         return "Sicily"
     
-    if answerNumber == 2:
+    elif answerNumber == 2:
         return "Rome, then Sicily."
     
     elif answerNumber == 3:
@@ -13,5 +13,13 @@ def travelDestination(answerNumber):
     elif answerNumber == 4:
         return "definitely Sicily."
 
-r = random.randint(1,5)
+r = random.randint(1,4)
+#Why does this work? Should I not define 1,5 here?
 plan=travelDestination(r)
+print(plan)
+
+def spam():
+    print(eggs)
+eggs = 42
+spam()
+#print(eggs) Book has this unnecessary additional line.
